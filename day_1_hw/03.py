d227b8bf74b516b93dd096cd4588b1d0354c8fbe
ljw <jjww-0312@naver.com>
2019-07-08 19:07:42 +0900

    sample

---
+++ day_1_hw/03.py
@@ -0,0 +1,19 @@
+while True:
+    cd = input('Enter the single nt : ')
+    if cd=="A":
+        print('Adenine')
+        break
+    elif cd=="C":
+        print('Cytosine')
+        break
+    elif cd=="G":
+        print('Guanine')
+        break
+    elif cd=="T":
+        print('Thymine')
+        break
+    elif cd=="U":
+        print('Uracil')
+        break
+
+
