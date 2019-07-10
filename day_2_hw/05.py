19214b0b4eced245c831c60912de3b15b5700692
ljw <jjww-0312@naver.com>
2019-07-10 12:10:07 +0900

    05 success

--- day_2_hw/05.py
+++ day_2_hw/05.py
@@ -2,18 +2,18 @@ import sys
 
 def k_mer(l1,l2,n):
     if n==1:
-        l2
+        return l2
     while True:
         if len(l2[0]) ==n:
             break
         j = l2.pop(0)
         for i in l1:
             l2.append(j+i)
-    k_mer(l1,l2,n-1)
+    return k_mer(l1,l2,n-1)
 
 l1 = ["A","T","G","C"]
 l2 = ["A","T","G","C"]
 n = int(sys.argv[1])
 
-k_mer(l1,l2,n)
+print(k_mer(l1,l2,n))
 
--- day_2_hw/05_2.py
+++ day_2_hw/05_2.py
@@ -2,16 +2,16 @@ import sys
 
 def k_mer(l1,l2,n):
     if n==1:
-        l2
+        return l2
     lt = []
     for j in l2:
         for i in l1:
             lt.append(j+i)
-    k_mer(l1,lt,n-1)
+    return k_mer(l1,lt,n-1)
 
 l1 = ["A","T","G","C"]
 l2 = ["A","T","G","C"]
 n = int(sys.argv[1])
 
-k_mer(l1,l2,n)
+print(k_mer(l1,l2,n))
 
